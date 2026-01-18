50d661e6150873a3818106e7b2b23671b49c22a6
Moxvallix Mox <moxvallix@gmail.com>
2026-01-18 17:21:50 +1030

    feat(auth): implement oauth service for ncrs

---
+++ extensions/needcoolershoes/oauth/auth/provider/oauth/service/ncrs.php
@@ -0,0 +1,117 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+
+namespace needcoolershoes\oauth\auth\provider\oauth\service;
+
+use needcoolershoes\oauth\oauth\oauth2\service\NCRSService;
+
+/**
+* NCRS OAuth service
+*/
+class ncrs extends \phpbb\auth\provider\oauth\service\base
+{
+    /**
+    * phpBB config
+    *
+    * @var \phpbb\config\config
+    */
+    protected $config;
+
+    /**
+    * phpBB request
+    *
+    * @var \phpbb\request\request_interface
+    */
+    protected $request;
+
+    /**
+    * Constructor
+    *
+    * @param    \phpbb\config\config               $config
+    * @param    \phpbb\request\request_interface   $request
+    */
+    public function __construct(\phpbb\config\config $config, \phpbb\request\request_interface $request)
+    {
+        $this->config = $config;
+        $this->request = $request;
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function get_service_credentials()
+    {
+        return array(
+            'key'     => $this->config['auth_oauth_ncrs_key'],
+            'secret'  => $this->config['auth_oauth_ncrs_secret'],
+        );
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function perform_auth_login()
+    {
+        if (!($this->service_provider instanceof NCRSService))
+        {
+            throw new \phpbb\auth\provider\oauth\service\exception('AUTH_PROVIDER_OAUTH_ERROR_INVALID_SERVICE_TYPE');
+        }
+
+        error_log($this->request->variable('code', ''), 0);
+
+        // This was a callback request from ncrs, get the token
+        $this->service_provider->requestAccessToken($this->request->variable('code', ''));
+
+        // Send a request with it
+        $result = json_decode($this->service_provider->request('profile'), true);
+
+        // Return the unique identifier returned from ncrs
+        error_log($result['id'], 0);
+        return $result['id'];
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function perform_token_auth()
+    {
+        if (!($this->service_provider instanceof NCRSService))
+        {
+            throw new \phpbb\auth\provider\oauth\service\exception('AUTH_PROVIDER_OAUTH_ERROR_INVALID_SERVICE_TYPE');
+        }
+
+        // Send a request with it
+        $result = json_decode($this->service_provider->request('profile'), true);
+
+        // Return the unique identifier returned from ncrs
+        return $result['id'];
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function get_external_service_class()
+    {
+        return NCRSService::class;
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function get_auth_scope()
+    {
+        return [
+            'read',
+        ];
+    }
+}
---
+++ extensions/needcoolershoes/oauth/oauth/oauth2/service/NCRSService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace needcoolershoes\oauth\oauth\oauth2\service;
+
+use OAuth\Common\Consumer\CredentialsInterface;
+use OAuth\Common\Http\Client\ClientInterface;
+use OAuth\Common\Http\Exception\TokenResponseException;
+use OAuth\Common\Http\Uri\Uri;
+use OAuth\Common\Http\Uri\UriInterface;
+use OAuth\Common\Storage\TokenStorageInterface;
+use OAuth\OAuth2\Token\StdOAuth2Token;
+use OAuth\OAuth2\Service\AbstractService as OAuth2Service;
+
+class NCRSService extends OAuth2Service
+{
+    const SCOPE_READ = 'read';
+
+    public function __construct(
+        CredentialsInterface $credentials,
+        ClientInterface $httpClient,
+        TokenStorageInterface $storage,
+        $scopes = [],
+        ?UriInterface $baseApiUri = null
+    ) {
+        parent::__construct($credentials, $httpClient, $storage, $scopes, $baseApiUri);
+
+        if (null === $baseApiUri) {
+            $this->baseApiUri = new Uri('http://192.168.1.32:3000/api/v1/');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthorizationEndpoint()
+    {
+        return new Uri('http://192.168.1.32:3000/oauth/authorize');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAccessTokenEndpoint()
+    {
+        return new Uri('http://192.168.1.32:3000/oauth/token');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthorizationMethod()
+    {
+        return static::AUTHORIZATION_METHOD_HEADER_BEARER;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function parseAccessTokenResponse($responseBody)
+    {
+        $data = json_decode($responseBody, true);
+        error_log($responseBody, 0);
+
+        if (null === $data || !is_array($data)) {
+            throw new TokenResponseException('Unable to parse response.');
+        } elseif (isset($data['error'])) {
+            throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
+        }
+
+        $token = new StdOAuth2Token();
+        $token->setAccessToken($data['access_token']);
+        $token->setLifetime($data['expires_in']);
+
+        if (isset($data['refresh_token'])) {
+            $token->setRefreshToken($data['refresh_token']);
+            unset($data['refresh_token']);
+        }
+
+        unset($data['access_token'], $data['expires_in']);
+
+        $token->setExtraParams($data);
+
+        return $token;
+    }
+}
